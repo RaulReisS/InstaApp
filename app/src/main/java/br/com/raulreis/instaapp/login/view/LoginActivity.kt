5640ef6c1deb05c9f1646dfe3c20f7ef77d91c8c
Raul Reis <raul.soares@usp.br>
2023-09-04 20:20:50 -0300

    Criado a estrutura de projeto, assim como seu estilo e seu incluído seus arquivos de recurso de imagem (XML e PNG)

---
+++ app/src/main/java/br/com/raulreis/instaapp/login/view/LoginActivity.kt
@@ -0,0 +1,12 @@
+package br.com.raulreis.instaapp.login.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.raulreis.instaapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
