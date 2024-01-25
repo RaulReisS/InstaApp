66a005001887f23b602845eddb2fb756f740917b
Raul Reis <raul.soares@usp.br>
2024-01-24 21:57:47 -0300

    Ajustando os contadores de seguidores e seguidos utilizando o Firebase

--- app/src/main/java/br/com/raulreis/instaapp/profile/data/FireProfileDataSource.kt
+++ app/src/main/java/br/com/raulreis/instaapp/profile/data/FireProfileDataSource.kt
@@ -96,6 +96,8 @@ class FireProfileDataSource : ProfileDataSource {
             else FieldValue.arrayRemove(uid)
             )
             .addOnSuccessListener { res ->
+                followingCounter(uid, isFollow)
+                followersCount(userUUID)
                 callback.onSuccess(true)
             }
             .addOnFailureListener { exception ->
@@ -110,7 +112,9 @@ class FireProfileDataSource : ProfileDataSource {
                                 "followers" to listOf(uid)
                             )
                         )
-                        .addOnSuccessListener {
+                        .addOnSuccessListener {res ->
+                            followingCounter(uid, isFollow)
+                            followersCount(userUUID)
                             callback.onSuccess(true)
                         }
                         .addOnFailureListener { exception ->
@@ -124,4 +128,33 @@ class FireProfileDataSource : ProfileDataSource {
                 callback.onComplete()
             }
     }
+
+    private fun followingCounter(uid: String, isFollow: Boolean) {
+        val meRef = FirebaseFirestore.getInstance()
+            .collection("/users")
+            .document(uid)
+
+        if (isFollow)
+            meRef.update("following", FieldValue.increment(1))
+        else
+            meRef.update("following", FieldValue.increment(-1))
+    }
+
+    private fun followersCount(uid: String) {
+        val meRef = FirebaseFirestore.getInstance()
+            .collection("/users")
+            .document(uid)
+
+        FirebaseFirestore.getInstance()
+            .collection("followers")
+            .document(uid)
+            .get()
+            .addOnSuccessListener { response ->
+                if (response.exists()) {
+                    val list = response.get("followers") as List<String>
+                    meRef.update("followers", list.size)
+                }
+            }
+    }
+
 }
